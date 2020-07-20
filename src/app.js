0e4e8dac885b696e9821b164add534e72071422a
Eliezer Cútalo <eliezercsj@gmail.com>
2020-07-19 22:10:07 -0300

    feat: Habilitando CORS

--- src/app.js
+++ src/app.js
@@ -20,8 +20,18 @@ const productRoute = require('./routes/product-route')
 const customerRoute = require('./routes/customer-routes')
 const orderRoute = require('./routes/order-routes')
 
-app.use(bodyParser.json())
+app.use(bodyParser.json({
+    limit: '5mb'
+}));
 app.use(bodyParser.urlencoded({extended: false}))
+
+//Habilita o CORS
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 
 app.use('/', indexRoute);
 app.use('/products', productRoute);
